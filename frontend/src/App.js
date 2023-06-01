ac7ef50ab9e0ebef66f339c5e90ad35374be53eb
iAmKoldyn <pataraya951@gmail.com>
2023-06-01 23:50:11 +0300

    the ability to put a geo-tag and basic positioning has been added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import "./index.css";
+import { BrowserRouter as Router } from 'react-router-dom';
 import Map from "./components/map/map";
 
 function App() {
-  return (
-      <div className="App">
-        <div className="main__wrapper">
-          <Map />
-        </div>
-      </div>
-  );
+    return (
+        <Router>
+            <Map />
+        </Router>
+    );
 }
 
 export default App;
--- frontend/src/components/map/map.js
+++ frontend/src/components/map/map.js
@@ -1,8 +1,61 @@
-import React from 'react';
-import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import React, { useState, useEffect } from 'react';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-graticule';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './index.css';
+
 const Map = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [markers, setMarkers] = useState([]);
+    const [labelPosition, setLabelPosition] = useState(null);
+    const [showLabel, setShowLabel] = useState(false);
+
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const savedMarkers = params.get('markers');
+        if (savedMarkers) {
+            setMarkers(JSON.parse(savedMarkers));
+        }
+    }, [location.search]);
+
+    const MapEvents = () => {
+        const map = useMapEvents({
+            click: (e) => {
+                const newMarker = e.latlng;
+                setMarkers((oldMarkers) => {
+                    const updatedMarkers = [...oldMarkers, newMarker];
+                    navigate(`?markers=${JSON.stringify(updatedMarkers)}`);
+                    return updatedMarkers;
+                });
+            },
+            mousemove: (e) => {
+                setLabelPosition(e.latlng);
+            },
+            dblclick: () => {
+                setShowLabel(!showLabel);
+            },
+            map: {
+                whenReady: (map) => {
+                    L.latlngGraticule({
+                        font: '12px Sans-Serif',
+                        color: '#333',
+                        opacity: 0.8,
+                        showLabel: true,
+                        zoomInterval: [
+                            {start: 2, end: 3, interval: 30},
+                            {start: 4, end: 4, interval: 10},
+                            {start: 5, end: 7, interval: 5},
+                            {start: 8, end: 10, interval: 1}
+                        ]
+                    }).addTo(map);
+                }
+            }
+        });
+        return null;
+    };
 
     return (
         <MapContainer
@@ -15,11 +68,19 @@ const Map = () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br/> Easily customizable.
-                </Popup>
-            </Marker>
+            {markers.map((position, idx) =>
+                <Marker key={idx} position={position}>
+                    {(showLabel && position === markers[markers.length - 1]) && (
+                        <Popup>Lat: {position.lat}, Lng: {position.lng}</Popup>
+                    )}
+                </Marker>
+            )}
+            {labelPosition && showLabel && (
+                <Marker position={labelPosition}>
+                    <Popup>Lat: {labelPosition.lat}, Lng: {labelPosition.lng}</Popup>
+                </Marker>
+            )}
+            <MapEvents />
         </MapContainer>
     );
 };
